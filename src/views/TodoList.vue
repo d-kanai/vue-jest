a89d5da0a249baddc7f4a7439f16fc81c47ce1c5
d-kanai <daiki.kanai31@gmail.com>
2022-01-06 12:20:57 +0900

    init

---
+++ src/views/TodoList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="todoList">
+    hello todo list
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TodoList",
+});
+</script>
