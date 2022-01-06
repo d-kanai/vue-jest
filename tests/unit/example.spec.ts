f864b5d80a005a758f7c05c7be36bc4e0f97d641
d-kanai <daiki.kanai31@gmail.com>
2022-01-06 13:10:08 +0900

    init

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils";
 import Sample from '@/views/Sample.vue'
 
 describe("Sample.vue", () => {
-  it("should show sample string", () => {
+  it("xxx", () => {
     const wrapper = mount(Sample)
     expect(wrapper.text()).toMatch("Sample");
   });
