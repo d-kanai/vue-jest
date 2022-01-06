1ea15cb315fa99f4221910a752defdc3502ba634
d-kanai <daiki.kanai31@gmail.com>
2022-01-06 12:41:19 +0900

    init

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,9 @@
 import { mount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = mount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("Sample.vue", () => {
+  it("xxx", () => {
+    const wrapper = mount(HelloWorld)
+    expect(wrapper.text()).toMatch("guide");
   });
 });
