e5c519e61bd2ee13519a0293df4278fbbcc97a6f
d-kanai <daiki.kanai31@gmail.com>
2022-01-14 21:09:23 +0900

    minor fix

--- src/apis/TodoApi.ts
+++ src/apis/TodoApi.ts
@@ -2,9 +2,9 @@ export async function getTodoList() {
   // await api call
   console.log('api call: getTodoList')
   return [
-    { id: 1, name: "todo item" },
-    { id: 2, name: "todo item2" },
-    { id: 3, name: "clean room" }
+    { id: 1, name: "todo item", status: true },
+    { id: 2, name: "todo item2", status: false },
+    { id: 3, name: "clean room", status: false }
   ];
 }
 
---
+++ src/views/NewComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <input type="checkbox" v-model="todo.status" @change="this.updateStatus(todo)" />
+  <router-link
+    :to="{name: 'Todo', params: {id: todo.id}}"
+
+  >
+    <span>{{ todo.name }}</span>
+  </router-link>
+</template>
+<script>
+export default {
+  name: "NewComponent",
+  props: {
+    todo: {}
+  }
+};
+</script>
