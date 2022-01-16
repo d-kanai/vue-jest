8b03868ce64b82f3f34ff53b24b52e13bdf86660
d-kanai <daiki.kanai31@gmail.com>
2022-01-16 17:36:21 +0900

    text field

---
+++ src/views/TextField.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label>{{label}}</label>
+    <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "TextField",
+  props: {
+    modelValue: {},
+    label: {}
+  },
+};
+</script>
--- src/views/TodoNew.vue
+++ src/views/TodoNew.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <p>New Todo</p>
+    <form @submit.prevent>
+      <TextField v-model="todo.title" label="title" />
+      <TextField v-model="todo.assignee" label="assignee" />
+      <button @click="this.submit">submit</button>
+    </form>
   </div>
-
 </template>
 
 <script>
+import TextField from "@/views/TextField";
+
 export default {
+  components: { TextField },
+  data() {
+    return {
+      todo: {
+        title: "",
+        assignee: "",
+      },
+    };
+  },
+  methods: {
+    async submit() {
+      console.log(this.todo);
+      return false;
+    },
+  },
 };
 </script>
-
-<style scoped>
-
-</style>
