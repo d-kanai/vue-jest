c20e0ed16a8e43666b1621a60e34aacaaaad4eee
d.kanai <daiki.kanai31@gmail.com>
2022-02-23 12:44:11 +0900

    hook test

--- tests/unit/useCustomLineChart.spec.ts
+++ tests/unit/useCustomLineChart.spec.ts
@@ -1,7 +1,13 @@
 import { useCustomLineChart } from "@/hooks/useCustomLineChart";
+import { flushPromises } from "@vue/test-utils";
 
 describe("useCustomLineChart", () => {
-  it("xxx", () => {
-    useCustomLineChart()
+  it("xxx", async () => {
+    //when
+    const actual = useCustomLineChart()
+    await flushPromises();
+    //then
+    expect(actual.chartDataList.value.length).toEqual(2)
+    expect(actual.chartDataList.value[0].chartData.datasets[0].label).toEqual('Long Method')
   });
 });
