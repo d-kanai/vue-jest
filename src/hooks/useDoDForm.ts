8cc6f239772f6d940fd956bbf103980954f7f94f
d.kanai <daiki.kanai31@gmail.com>
2022-03-05 21:32:34 +0900

    missing add

---
+++ src/hooks/useDoDForm.ts
@@ -0,0 +1,12 @@
+import * as yup from "yup";
+import { useField, useForm } from "vee-validate";
+
+export const useDoDForm = () => {
+  const { handleSubmit, errors } = useForm({
+    validationSchema: yup.object({
+      name: yup.string().required("name is required")
+    }),
+  });
+  const name = useField("name").value
+  return { handleSubmit, errors, name}
+}
