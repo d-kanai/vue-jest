fa84360afda6d89894b969e09603d774bfc05375
d.kanai <contact@dkanai.com>
2022-03-17 23:01:33 +0900

    minor refactor

--- tests/unit/views/dodDetail.spec.ts
+++ tests/unit/views/dodDetail.spec.ts
@@ -11,21 +11,21 @@ describe("DoDList.vue", () => {
   describe("CreateDoDRecord", () => {
     it("should create dod record", async function () {
       //given
-      const dodList = mockDoDListApi();
+      const dodId = 1234
       const mockCreateDoDRecordApi = jest.spyOn(api, "createDoDRecord").mockImplementation(jest.fn());
-     //when
-      const wrapper = await mountWithFlushPromise(DoDDetail, `/dods/${dodList.items[0].id}`);
+      //when
+      const wrapper = await mountWithFlushPromise(DoDDetail, `/dods/${dodId}`);
       await wrapper.find("#input-date").setValue("2022-01-01");
       await wrapper.find("#input-value").setValue("30");
       await wrapper.find("#input-comment").setValue("Add new feature");
-      // then
+      //then
       await wrapper.vm.onDoDRecordSubmit();
       expect(mockCreateDoDRecordApi).toHaveBeenCalledTimes(1);
       expect(mockCreateDoDRecordApi).toHaveBeenCalledWith({
         comment: "Add new feature",
         date: "2022-01-01",
         value: "30",
-        dodId: dodList.items[0].id,
+        dodId: dodId,
       });
       expect(wrapper.text()).toMatch("2022-01-01");
       expect(wrapper.text()).toMatch("30");
