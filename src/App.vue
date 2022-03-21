885c84cf3cce4ac1183704ffa21e650e1a2c6b0a
d.kanai <contact@dkanai.com>
2022-03-22 08:27:48 +0900

    extract component

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,17 @@
 <template>
-  <!-- @IMPROVE make header component -->
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="/">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          width="112"
-          height="28"
-        />
-      </a>
-
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <div class="navbar-item">
-          <router-link to="/">Top</router-link>
-        </div>
-        <div class="navbar-item">
-          <router-link to="/dods">DoD</router-link>
-        </div>
-      </div>
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light"> Log in </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
-  <router-view />
+  <div>
+    <Header />
+    <router-view />
+  </div>
 </template>
+
+<script lang='ts'>
+import Header from "@/components/Header.vue";
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: "App",
+  components: { Header },
+})
+</script>
 
 <style lang="scss"></style>
