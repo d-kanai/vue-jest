8c885696fbc217142bf67efd1fb4d104f9a12bc1
d.kanai <contact@dkanai.com>
2022-03-22 20:25:48 +0900

    @IMPROVE

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+//@IMPROVE: use env var
 export const http = axios.create({
+  //baseURL: "http://54.199.13.127:9000",
   baseURL: "http://localhost:9000",
   headers: {
     "Content-Type": "application/json;charset=utf-8",
